4e994cd99017ea7043953466489b0249c7fad41c
Onur Ulusoy <onurulusoys4@gmail.com>
2023-01-31 21:56:18 +0300

    Draft bluetooth .NET interface created.

---
+++ Bluetooth Interface (.NET)/Bluetooth Interface/Bluetooth Interface/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Bluetooth_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 77);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(485, 229);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.DeviceListSelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.StartScanClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop Scanning";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.StopScanClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(384, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Disconnect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.DisconnectClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(265, 22);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 35);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Connect";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.ConnectClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 325);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Bluetooth Interface (.NET)/Bluetooth Interface/Bluetooth Interface/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bluetooth_Interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1Load(object sender, EventArgs e)
+        {
+
+        }   
+
+        private void StartScanClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StopScanClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ConnectClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DisconnectClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeviceListSelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
